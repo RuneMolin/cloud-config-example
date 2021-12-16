6ce0670e5f513b0ce3709c676e8dc58b3727f3e3
Rune Molin <rmo@nine.dk>
2021-12-16 09:27:17 +0100

    refactor client

---
+++ client/src/main/java/dk/nine/cloudconfigclient/Controller.java
@@ -0,0 +1,23 @@
+package dk.nine.cloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Rune Molin, rmo@nine.dk
+ **/
+@RestController
+@RefreshScope
+public class Controller {
+  @Value("${message}")
+  private String message;
+
+  @GetMapping(value = "/message", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String getMessage() {
+    return message;
+  }
+
+}
