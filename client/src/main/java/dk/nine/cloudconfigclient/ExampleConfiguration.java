226067d9dabbd566d7853f1c1f16f9bd0196fe82
Rune Molin <rmo@nine.dk>
2021-12-16 16:14:00 +0100

    refreshscope not needed on configurations

--- client/src/main/java/dk/nine/cloudconfigclient/ExampleConfiguration.java
+++ client/src/main/java/dk/nine/cloudconfigclient/ExampleConfiguration.java
@@ -1,13 +1,11 @@
 package dk.nine.cloudconfigclient;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Rune Molin, rmo@nine.dk
  **/
-@RefreshScope
 @Configuration
 @ConfigurationProperties(prefix = "example")
 public class ExampleConfiguration {
